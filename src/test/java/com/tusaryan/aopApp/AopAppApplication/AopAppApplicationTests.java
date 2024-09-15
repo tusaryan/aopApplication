25d3ee3c901228f05945ca8fa5a99d874424cd52
Aryan Kumar <aryankumar79922@gmail.com>
2024-09-15 18:41:59 +0530

    basic structure

---
+++ src/test/java/com/tusaryan/aopApp/AopAppApplication/AopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusaryan.aopApp.AopAppApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
